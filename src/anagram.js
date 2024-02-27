5c52ec4ba621e23d312a1ddd08d76a27c28c7227
Will Williams <wwilliams@indeed.com>
2024-02-27 12:36:15 -0800

    Implements Anagram()

--- src/anagram.js
+++ src/anagram.js
@@ -3,6 +3,20 @@
  * @param {string} s1
  * @param {string} s2
  */
-function anagram(s1, s2) {}
+function anagram(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+  // Check if the lengths of the strings are different
+  if (s1.length !== s2.length) {
+    return false;
+  }
+
+  // Convert strings to arrays, sort them, and join back to strings
+  const sortedS1 = s1.split("").sort().join("");
+  const sortedS2 = s2.split("").sort().join("");
+
+  // Compare the sorted strings
+  return sortedS1 === sortedS2;
+}
 
 module.exports = anagram;
