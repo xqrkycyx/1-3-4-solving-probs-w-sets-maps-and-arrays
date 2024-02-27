c720f8e6b3d0ececa69bdef1149e49fc056f042d
Will Williams <wwilliams@indeed.com>
2024-02-27 12:37:34 -0800

    Implements anagramGroups()

--- src/anagramGroups.js
+++ src/anagramGroups.js
@@ -2,6 +2,26 @@
  * Return a grouping of words by whether they are anagrams of each other or not
  * @param {array} words to be grouped
  */
-function anagramGroups(words) {}
+function anagramGroups(words) {
+  const anagramMap = new Map();
+
+  // Iterate through each word
+  for (const word of words) {
+    // Sort the characters of the word
+    const sortedWord = word.split("").sort().join("");
+
+    // Check if the sorted word exists in the map
+    if (anagramMap.has(sortedWord)) {
+      // Add the word to the existing anagram group
+      anagramMap.get(sortedWord).push(word);
+    } else {
+      // Create a new anagram group with the sorted word
+      anagramMap.set(sortedWord, [word]);
+    }
+  }
+
+  // Convert map values to array of arrays
+  return Array.from(anagramMap.values());
+}
 
 module.exports = anagramGroups;
