76a8b6ccf6a31889283cdb8432b59648e3e32b91
Will Williams <wwilliams@indeed.com>
2024-02-27 12:41:49 -0800

    Implements first single character

--- src/firstSingleCharacter.js
+++ src/firstSingleCharacter.js
@@ -2,6 +2,23 @@
  * Return the first character in the string that occurs only once.
  * @param {string} word the string to be analysed
  */
-function firstSingleCharacter(word) {}
+function firstSingleCharacter(word) {
+  const charCountMap = new Map();
+
+  // Count occurrences of each character in the word
+  for (const char of word) {
+    charCountMap.set(char, (charCountMap.get(char) || 0) + 1);
+  }
+
+  // Find the first character with a count of 1
+  for (const char of word) {
+    if (charCountMap.get(char) === 1) {
+      return char;
+    }
+  }
+
+  // If no single characters are found, return null
+  return null;
+}
 
 module.exports = firstSingleCharacter;
